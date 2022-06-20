f2dfb8bfe28b6db5befd706685fe9b3ddbdae83e
Ariana Maldonado <arii.maldonado97@gmail.com>
2022-06-20 16:06:44 -0300

    BaSP-QA: login first test

--- test/specs/login.spec.js
+++ test/specs/login.spec.js
@@ -1,12 +1,17 @@
-const LoginPage = request('../pageobjects/login.page')
+const LoginPage = request('../pageobjects/login.page.js');
+
 describe('Login page testing', () => {
 
     beforeAll('open browser', () => {
         browser.url('https://www.saucedemo.com/');
     });
     it('Login success', async () => {
-        await LoginPage.inputUsername.setValue('standard_user');
-        await LoginPage.inputPassword.setValue('secret_sauce');
-        await LoginPage.btnLogin.click();
+        await LoginPage.login('standard_user', 'secret_sauce');
+    });
+    describe('Username input test', () => {
+        it('empty username should display error', async () => {
+            await LoginPage.login('', 'secret_sauce');
+            await expect(LoginPage.errorContainer).toHaveText('Epic sadface: Username is required');
+        });
     });
 });
