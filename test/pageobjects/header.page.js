27bf210eaa4ffa0ea61f6c77f93f76278c5964c4
Ariana Maldonado <arii.maldonado97@gmail.com>
2022-06-21 19:39:07 -0300

    BaSP-QA: header page objects

--- test/pageobjects/header.page.js
+++ test/pageobjects/header.page.js
@@ -1,23 +1,27 @@
-// class Header {
-//     //Getters
-//     get headerImg() { return $('') };
-//     get burgerBtn() { return $('') };
-//     get sidebar() { return $('') };
-//     get allItemsSidebar() { return $('') };
-//     get aboutSidebar() { return $('') };
-//     get logoutSidebar() { return $('') };
-//     get resetAppStateSidebar() { return $('') };
-//     get closeSidebar() { return $('') };
+class Header {
+    //Getters
+    get headerLogo() { return $('#header_container > div.primary_header > div.header_label > div') };
+    get burgerBtn() { return $('#react-burger-menu-btn') };
+    get sidebar() { return $('#menu_button_container > div > div.bm-menu-wrap > div.bm-menu > nav') };
+    get allItemsSidebar() { return $('#inventory_sidebar_link') };
+    get aboutSidebar() { return $('#about_sidebar_link') };
+    get logoutSidebar() { return $('#logout_sidebar_link') };
+    get resetAppStateSidebar() { return $('#reset_sidebar_link') };
+    get closeSidebar() { return $('#react-burger-cross-btn') };
+    get cartBtn() { return $('#shopping_cart_container > a') };
 
     //Methods
-    // async clickMenu () {
-    //     await this.headerImg.click();
-    //     await this.burgerBtn.click();
-    //     await this.sidebar.click();
-    //     await this.allItemsSidebar.click();
-    //     await this.aboutSidebar.click();
-    //     await this.logoutSidebar.click();
-    //     await this.resetAppStateSidebar.click();
-    //     await this.closeSidebar.click();
-    // };
+    async clickMenu () {
+        await this.headerLogo.click();
+        await this.burgerBtn.click();
+        await this.sidebar.click();
+        await this.allItemsSidebar.click();
+        await this.aboutSidebar.click();
+        await this.logoutSidebar.click();
+        await this.resetAppStateSidebar.click();
+        await this.closeSidebar.click();
+        await this.cartBtn.click();
+    };
 };
+
+module.exports = new Header();
